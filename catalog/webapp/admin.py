23711883fc598cf2f06d4cd74e36be670b145ed8
Archer <arseniy.tarapon@gmail.com>
2019-04-06 14:56:53 +0600

    1. Создание моделей и их регистрация в админке.

--- catalog/webapp/admin.py
+++ catalog/webapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from webapp.models import Product, Photo, Category, Order
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'receipt_date']
+    ordering = ['-receipt_date']
+    search_fields = ['name', 'id']
+
+
+def list_admin_with_pk(*fields):
+    class PkListAdmin(admin.ModelAdmin):
+        list_display = ['pk'] + list(fields)
+    return PkListAdmin
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Photo, list_admin_with_pk('photo'))
+admin.site.register(Category, list_admin_with_pk('name'))
+admin.site.register(Order, list_admin_with_pk('user'))
+
--- catalog/webapp/models.py
+++ catalog/webapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    receipt_date = models.DateField()
+    categories = models.ManyToManyField('Category', related_name='categories', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='photo', null=True, blank=True)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ManyToManyField('Product', related_name='product')
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True)
+    comment = models.TextField(max_length=2000, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
